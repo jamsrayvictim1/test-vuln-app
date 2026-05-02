50f023dd24039a423b66a64a122970a4033c5ff7
alexander anderson <bigraga+victim@wearehackerone.com>
2026-05-02 19:12:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,8 @@
 'use strict';
-/* let Age;
-const ageRequired = 18;
+const sink = document.getElementById("flaw");
+const click = document.getElementById("click");
+const input = document.querySelector("#input");
 
-function allowedMarry(Age) {
-  if(Age=>ageRequired) {
-    console.log("can get married")
-  } else {
-    console.log("cannot get married")
-  }
-};
-
-// calling / running the function
-allowedMarry(21); */
-
-// parameters and arguments are different
-
-// parameters is mangos and apples
-function fruitJuice(mangos, apples) {
-  console.log(mangos, apples);
-  const juice = `this juice created with ${mangos} and ${apples}`;
-  return juice;
-};
-
-// arguments is 2, 4 or parameter's value when functions executed/called
-fruitJuice(2, 4);
-console.log(fruitJuice(2, 4));
+function render() {
+  sink.innerHTML = input.value;
+}
